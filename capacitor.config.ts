ef528036ab810091249779ea43bf9d4b6cb321bd
krelk-works <leo.craciun97@gmail.com>
2025-09-20 18:12:45 +0200

    Add capacitor to project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.krelk.financetracker',
+  appName: 'FinanceTracker-App',
+  webDir: 'dist'
+};
+
+export default config;
